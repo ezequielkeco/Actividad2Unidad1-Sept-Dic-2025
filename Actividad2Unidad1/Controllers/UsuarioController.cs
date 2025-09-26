8307db7a0e93396d7fb8ffed96dc0de692a5236a
Ezequiel Santos <ezequielsantosg@gmail.com>
2025-09-25 22:20:00 -0400

    Primer ejercicio - Calcular edad y año de nacimiento

---
+++ Actividad2Unidad1/Controllers/UsuarioController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad2Unidad1.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [HttpGet]
+        public IActionResult Datos()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Datos(string nombre, DateTime fechaNacimiento)
+        {
+            int edad = CalcularEdad(fechaNacimiento);
+            string anioNacimiento = fechaNacimiento.ToString("yy");
+
+            ViewBag.Nombre = nombre;
+            ViewBag.Edad = edad;
+            ViewBag.AnioNacimiento = anioNacimiento;
+
+            return View();
+        }
+
+        private int CalcularEdad(DateTime fechaNacimiento)
+        {
+            int edad = DateTime.Now.Year - fechaNacimiento.Year;
+            if (DateTime.Now.DayOfYear < fechaNacimiento.DayOfYear)
+                edad--;
+
+            return edad;
+        }
+    }
+}
