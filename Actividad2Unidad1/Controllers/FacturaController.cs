6813af0807221a1b326dc09de1a03b5557540d20
Ezequiel Santos <ezequielsantosg@gmail.com>
2025-09-26 01:15:46 -0400

    Quinto ejercicio - Factura

---
+++ Actividad2Unidad1/Controllers/FacturaController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad2Unidad1.Controllers
+{
+    public class FacturaController : Controller
+    {
+        [HttpGet]
+        public IActionResult Facturar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calcular(decimal monto, string opcion)
+        {
+            decimal subtotal = monto;
+            decimal impuesto = 0;
+            decimal descuento = 0;
+            decimal total = monto;
+
+            if (opcion == "ITBIS")
+            {
+                impuesto = monto * 0.18m;
+                total = subtotal + impuesto;
+            }
+            else if (opcion == "DESCUENTO")
+            {
+                descuento = monto * 0.15m;
+                total = subtotal - descuento;
+            }
+
+            ViewBag.Subtotal = "RD$ " + subtotal.ToString("N2");
+            ViewBag.Impuesto = impuesto > 0 ? "RD$ " + impuesto.ToString("N2") : "-";
+            ViewBag.Descuento = descuento > 0 ? "RD$ " + descuento.ToString("N2") : "-";
+            ViewBag.Total = "RD$ " + total.ToString("N2");
+
+            return View("Facturar");
+        }
+    }
+}
+
